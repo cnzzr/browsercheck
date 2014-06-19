3868145bffe70f185c1dfe5e9835369e66631937
02166 <zzr@msdi.cn>
2014-06-19 10:14:13 +0800

    基本实现各项功能

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
 ﻿//检测过程中需要使用到的资源下载地址，如果IE8、自动修复工具等
 
 
+//检查欺项的中文名称
+var CheckItemLang = {};
+CheckItemLang["TrustWebsite"] = "是否可信站点";
+CheckItemLang["Popup"] = "允许弹出窗口";
+CheckItemLang["Cookies"] = "允许Cookies";
+CheckItemLang["Javascript"] = "允许JS脚本";
+CheckItemLang["Flash"] = "安装Flash播放器";
+CheckItemLang["Sliverlight"] = "安装Silverlight插件";
 
+//ActiveX信息
+var ActiveX = [];
 //检测其他域名的配置情况
 var Domains = [];
 Domains.push({
@@ -12,17 +22,18 @@ Domains.push({
     helpUrl:"http://xxsq.msdi.cn/" //设置帮助地址，一般为Discuz
 });
 
-//Domains.push({
-//    key: "Portal",
-//    name:"应用集成门户",
-//    checkUrl: "http://zny.msdi.cn/tools/bc/check.html",
-//    helpUrl: ""
-//});
+Domains.push({
+    key: "Portal",
+    name:"应用集成门户",
+    checkUrl: "http://zny.msdi.cn/tools/bc/check.html",
+    helpMsg: "电话技术支持3214",
+    helpUrl: ""
+});
 
-//Domains.push({
-//    key: "Mis",
-//    name:"综合管理信息系统",
-//    checkUrl: "http://172.22.67.20:8044/tools/bc/check.html",
-//    helpUrl: ""
-//});
+Domains.push({
+    key: "coa",
+    name:"新公文处理系统",
+    checkUrl: "http://mis.msdi.cn:8080/coa/axis2-admin/bc/check.html",
+    helpUrl: ""
+});
 
